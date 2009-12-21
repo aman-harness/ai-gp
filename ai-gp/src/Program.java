59a7f1f12a4bdfa020ae2e0ee8e3647a0f588ca8
internetboy0823 <internetboy0823@314826aa-ed65-11de-bf31-4dec8810c1c1>
2009-12-21 05:09:58 +0000

    first

---
+++ ai-gp/src/Program.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)ai-gp.java
+ *
+ * ai-gp application
+ *
+ * @author 
+ * @version 1.00 2009/12/21
+ */
+ 
+public class Program {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
